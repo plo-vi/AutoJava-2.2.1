9c4d90350d6b36c2da88d35c8628b2eea01458b8
Viktoriia Plotnikova <viktoriia287@gmail.com>
2021-04-02 22:16:09 +0300

    test updated v1

--- src/test/java/ru/netology/DeliveryTest.java
+++ src/test/java/ru/netology/DeliveryTest.java
@@ -17,10 +17,13 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Calendar;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryTest {
+
+//    private WebDriver driver;
 
     public String deliveryDate() {
         Calendar currentDate = Calendar.getInstance();
@@ -38,6 +41,13 @@ public class DeliveryTest {
     public void setUpTest() {
         open("http://localhost:9999");
     }
+//
+//    @AfterEach
+//    public void teardown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
 
     @Test
     void shouldAcceptTest() {
